8cd4cc512a025623ac2b4c4b7330ffdf92166368
Avery <averywoodlief@gmail.com>
2026-06-08 13:43:50 -0500

    little clean up

--- navigator/navigator.py
+++ navigator/navigator.py
@@ -1,10 +1,10 @@
 from navigator.obstacle import Obstacle
 from navigator.node import Node
 from itertools import product
-from math import floor, ceil, sqrt, dist, pi
+from math import floor, ceil, sqrt, dist#, pi
 from pygame import Rect
 from random import randint
-from itertools import combinations
+#from itertools import combinations
 #import pygame
 
 nint = lambda x: (floor(x + 0.5) + ceil((2*x - 1)/4) - floor((2*x - 1)/4) - 1) # nearest integer function
@@ -82,9 +82,8 @@ class Circle (BaseNavigationShape):
         super().__init__()
         self.center = center
         self.radius = radius
-        #self.get_integer_points()
         self.get_integer_border_points()
-        #print(self.nodes, self.center, self.radius)
+
 
     def collidepoint(self, point):
         x, y = self.center
@@ -109,8 +108,7 @@ class Circle (BaseNavigationShape):
                     self.nodes.append(Node(point))
 
     def get_integer_points(self):
-        #for point in product(range(-(self.radius**2), ((self.radius + 1)**2)), repeat=2):
-            #print(point)
+
         cx, cy = self.center
         for x in range(cx - self.radius, cx + self.radius + 1):
             for y in range(cy - self.radius, cy + self.radius + 1):
@@ -153,37 +151,33 @@ class Navigator:
         else:
             raise ValueError
 
-        self.current = self.start
-        self.search_radius = 50
-        self.exploration_radius = 100
-        self.greedy_radius = 50
-        self.center_x, self.center_y = self.current.point
-        self.radar = Circle(self.current.point, self.search_radius)
-        self.tolerance = 10
-        self.is_stuck = False
-        self.initial_dist = dist(self.current.point, self.target.point)
-        self.curr_dist_to_target = dist(self.current.point, self.target.point)
-        self.prev_dist_to_target = dist(self.current.point, self.target.point)
+        self.current = self.start                                                   
+        self.search_radius = 50                                                                                                     
+        self.exploration_radius = 100                                               
+        self.greedy_radius = 50                                                     
+        self.center_x, self.center_y = self.current.point                           
+        self.radar = Circle(self.current.point, self.search_radius)                 
+        self.tolerance = 10                                                         
+        self.is_stuck = False                                                       
+        self.initial_dist = dist(self.current.point, self.target.point)             
+        self.curr_dist_to_target = dist(self.current.point, self.target.point)      
+        self.prev_dist_to_target = dist(self.current.point, self.target.point)      
 
-        self.movement = self.curr_dist_to_target - self.prev_dist_to_target
-        self.recent_improvements = []
-        self.stuck_window = 10
-        self.min_window_improvement = 100
-        self.progress = 0
-        self.dist_moved = 0
-        self.stuck_counter = 0
-        self.dist_improvement = 0
-        self.optimizing_attempt_sum = 0
-        self.chosen_nodes = []
-        self.stuck_limit = 5
-        self.escaping = False
-        self.path = [self.start]
-        self.nodes_that_made_navigator_stuck = []
+        self.movement = self.curr_dist_to_target - self.prev_dist_to_target         
+        self.recent_improvements = []                                               
+        self.min_window_improvement = 50
+        self.stuck_window = 5                                                 
+        self.progress = 0                                                           
+        self.dist_moved = 0                                                         
+        self.stuck_counter = 0                                                      
+        self.dist_improvement = 0                                                   
+        self.stuck_limit = 2                                                        
+        self.escaping = False                                                      
+        self.path = [self.start]                                                   
+        self.nodes_that_made_navigator_stuck = []                                 
 
-        #self.radar_surface = pygame.Surface((2*self.search_radius, 2*self.search_radius))
-        #self.radar_surface.fill((0, 0, 0))        
-        self.next_point_for_drawing = self.current
-        self.step_count = 0
+        self.next_point_for_drawing = self.current                                 
+        self.step_count = 0                                                        
 
     def get_neighbors_of_node(self, node, stride):
         saved = self.current
@@ -224,8 +218,9 @@ class Navigator:
         self.current = next
 
         self.curr_dist_to_target = dist(self.current.point, self.target.point)
-
         self.dist_improvement = self.prev_dist_to_target - self.curr_dist_to_target
+        '''if self.dist_improvement < 0, then doing worse than before
+           if self.dist_improvement > 0, then now doing better'''
         self.movement = abs(self.dist_improvement)
         
 
@@ -261,8 +256,6 @@ class Navigator:
         return False
 
     def update_radar(self, stride):
-        #center_x = None
-        #center_y = None
         current_x = self.current.point[0]
         current_y = self.current.point[1]
         target_x = self.target.point[0]
@@ -282,10 +275,26 @@ class Navigator:
             self.radar = Circle((self.center_x, self.center_y), self.search_radius)
         return
 
+    def is_legal_southern_node(self, node):
+        line_southern_hem = Line(self.current.point, node.point)
+        southern_hem_collide = self.line_collides(line_southern_hem)
+        if ((not southern_hem_collide) 
+            and not (node in self.path)
+            and not (node in self.nodes_that_made_navigator_stuck)):
+            return True
+        return False
+
+    def is_legal_northern_node(self, node):
+        line_northern_hem = Line(self.current.point, node.point)
+        northern_hem_collide = self.line_collides(line_northern_hem)
+        if ((not northern_hem_collide) 
+            and not (node in self.path)
+            and not (node in self.nodes_that_made_navigator_stuck)):
+            return True
+        return False
+
     def careful_step(self, stride):
-        #nodes = self.get_neighbors_of_current(stride)
-        #if (self.step_count % 100 == 0):
-            #self.update_radar(stride) # direct neighbors of self.current are in radar now
+
         self.update_radar(stride)
         allowed_nodes = []
         self.next_point_for_drawing = self.current
@@ -299,33 +308,14 @@ class Navigator:
                 x_sols = line_intersection_circle_x(self.current.point, line_slope, self.search_radius)
                 end_points = get_points_from_solved_x(line_slope, x_sols, self.current.point)
 
-                line_southern_hem = Line(self.current.point, end_points[0])
-                line_northern_hem = Line(self.current.point, end_points[1])
-
-
-                southern_hem_collide = self.line_collides(line_southern_hem)
-                northern_hem_collide = self.line_collides(line_northern_hem)
-                
-                
-
                 southern_hem_node = Node(end_points[0])
-                nothern_hem_node = Node(end_points[1])
-
-                if ((not southern_hem_collide) 
-                    and not (southern_hem_node in self.path)
-                    and not (southern_hem_node in self.nodes_that_made_navigator_stuck)):
+                northern_hem_node = Node(end_points[1])
+                
+                if self.is_legal_southern_node(southern_hem_node):
                     allowed_nodes.append(southern_hem_node)
 
-                if ((not northern_hem_collide) 
-                    and not (nothern_hem_node in self.path)
-                    and not (nothern_hem_node in self.nodes_that_made_navigator_stuck)):
-                    allowed_nodes.append(nothern_hem_node)
-
-            '''
-            collide = self.node_collides(node) #if node collides then so will the line
-            if ((not collide) and not (node in self.path)):
-                allowed_nodes.append(node)
-            '''
+                if self.is_legal_northern_node(northern_hem_node):
+                    allowed_nodes.append(northern_hem_node)
         
         self.optimize_costs(allowed_nodes)
         return
@@ -346,36 +336,22 @@ class Navigator:
                 x_sols = line_intersection_circle_x(self.current.point, line_slope, self.search_radius)
                 end_points = get_points_from_solved_x(line_slope, x_sols, self.current.point)
 
-                line_southern_hem = Line(self.current.point, end_points[0])
-                line_northern_hem = Line(self.current.point, end_points[1])
-
-
-                southern_hem_collide = self.line_collides(line_southern_hem)
-                northern_hem_collide = self.line_collides(line_northern_hem)
-                
-                
-
                 southern_hem_node = Node(end_points[0])
-                nothern_hem_node = Node(end_points[1])
-
-                if ((not southern_hem_collide) 
-                    and not (southern_hem_node in self.path) 
-                    and not (southern_hem_node in self.nodes_that_made_navigator_stuck)):
+                northern_hem_node = Node(end_points[1])
+                
+                if self.is_legal_southern_node(southern_hem_node):
                     allowed_nodes.append(southern_hem_node)
 
-                if ((not northern_hem_collide) 
-                    and not (nothern_hem_node in self.path)
-                    and not (nothern_hem_node in self.nodes_that_made_navigator_stuck)):
-                    allowed_nodes.append(nothern_hem_node)
+                if self.is_legal_northern_node(northern_hem_node):
+                    allowed_nodes.append(northern_hem_node)
 
         next = allowed_nodes[randint(0, len(allowed_nodes)-1)]
         self.update_movement(next)
-        #self.optimize_improvements(allowed_nodes)
         return
 
     def step(self):
         try:
-            self.progress = (self.dist_moved / self.initial_dist) * 100
+            self.progress = ((self.initial_dist - self.dist_moved) / self.initial_dist) * 100
         except ZeroDivisionError:
             self.progress = "NA"
 
@@ -383,14 +359,10 @@ class Navigator:
             self.obstacle_free(stride=1)
         else:
             if self.is_stuck:
-                #saved_search_radius = self.search_radius
-                #self.search_radius = 70
                 self.search_radius = self.exploration_radius
                 
                 self.escaping = True
                 self.explore(stride=10)
-
-                #self.search_radius = saved_search_radius
 
             elif dist(self.current.point, self.target.point) > self.tolerance:
                 self.search_radius = self.greedy_radius
@@ -399,23 +371,18 @@ class Navigator:
                 self.search_radius = self.greedy_radius
                 self.careful_step(stride=1)
             
-        # update stuck state AFTER movement happens
+        # update stuck state after movement happens
         self.recent_improvements.append(self.dist_improvement)
 
         if len(self.recent_improvements) > self.stuck_window:
+            # discard oldest improvement to get a new one in
             self.recent_improvements.pop(0)
 
         if len(self.recent_improvements) == self.stuck_window:
             total_improvement = sum(self.recent_improvements)
 
-            if total_improvement < 0:
-                if (self.stuck_counter >= self.stuck_limit):
-                    self.is_stuck = True
-                    self.nodes_that_made_navigator_stuck.append(self.current)
-                    #self.recent_improvements = []
-                    self.stuck_counter = 0
-                else:
-                    self.stuck_counter += 1
+            if total_improvement < self.min_window_improvement:
+                self.update_stuck_status()
             else:
                 self.is_stuck = False
                 self.escaping = False
@@ -423,6 +390,14 @@ class Navigator:
 
         self.step_count += 1
         return
+
+    def update_stuck_status(self) -> None:
+        if (self.stuck_counter >= self.stuck_limit):
+            self.is_stuck = True
+            self.nodes_that_made_navigator_stuck.append(self.current)
+            self.stuck_counter = 0
+        else:
+            self.stuck_counter += 1
         
 
     def __str__(self):
@@ -489,6 +464,7 @@ class NavigatorLog:
 
     def change_nav(self, new_nav : Navigator) -> None:
         self.focused_nav = new_nav
+        self.step_counter = 0
         self.prev_dist = dist(self.focused_nav.current.point, self.focused_nav.target.point)
         self.curr_dist = dist(self.focused_nav.current.point, self.focused_nav.target.point)
         return
@@ -516,11 +492,6 @@ class NavigatorLog:
                 f'{"ESCAPING?:":<30}'
                 f'{f"{self.focused_nav.escaping}":>{self.chars_occupied}}\n'
             )
-
-            try:
-                self.string += f'{f"{self.focused_nav.chosen_nodes[0]}{self.focused_nav.chosen_nodes[1]}":>{self.chars_occupied}}\n'
-            except (IndexError):
-                pass
 
     def write_points(self) -> None:
         if (self.bits["WRITE CURRENT POINT"]):
